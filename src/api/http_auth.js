87db6a8f7049820edba2a2b2c571480780361955
ThuThuy237 <52254322+ThuThuy237@users.noreply.github.com>
2022-01-15 16:03:16 +0700

    update

--- src/api/http_auth.js
+++ src/api/http_auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import cookies from 'react-cookies';
-// export const DOMAIN = 'https://mybackenddjangodapp.herokuapp.com/';
-export const DOMAIN = 'http://127.0.0.1:8000/';
+export const DOMAIN = 'https://mybackenddjangodapp.herokuapp.com/';
+// export const DOMAIN = 'http://127.0.0.1:8000/';
 export const http_auth = axios.create({
     baseURL: DOMAIN,
     timeout: 3000,
